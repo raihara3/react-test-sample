9d49bce70b6b7b96d31f8742f27a19238dcd3842
raihara3 <raihara3@gmail.com>
2022-01-10 21:03:10 +0900

    add: useEffect test

---
+++ src/UseEffectRender.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+
+const UseEffectRender = () => {
+  const [user, setUser] = useState(null)
+
+  const fetchJson = async() => {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/users/1")
+    return res.data
+  }
+
+  useEffect(() => {
+    const fetchUser = async() => {
+      const user = await fetchJson()
+      setUser(user)
+    }
+    fetchUser()
+  }, [])
+
+  return (
+    <div>
+      {user ? (
+        <p>I am {user.name} : {user.email}</p>
+      ) : (
+        null
+      )}
+    </div>
+  )
+}
+
+export default UseEffectRender
+
